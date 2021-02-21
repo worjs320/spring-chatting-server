8dbedb59eeb072347131f8e2f060a3cc19261d64
worjs320 <worjs320@naver.com>
2021-02-21 23:48:51 +0900

    feat: Update socket server code

--- src/main/java/com/server/chatting/chat/ChatRoom.java
+++ src/main/java/com/server/chatting/chat/ChatRoom.java
@@ -1,33 +1,19 @@
 package com.server.chatting.chat;
 
-import lombok.Builder;
 import lombok.Getter;
-import org.springframework.web.socket.WebSocketSession;
-
-import java.util.HashSet;
-import java.util.Set;
+import lombok.Setter;
+import java.util.UUID;
 
 @Getter
+@Setter
 public class ChatRoom {
-    private final String roomId;
-    private final String name;
-    private final Set<WebSocketSession> sessions = new HashSet<>();
+    private String roomId;
+    private String name;
 
-    @Builder
-    public ChatRoom(String roomId, String name) {
-        this.roomId = roomId;
-        this.name = name;
-    }
-
-    public void handleActions(WebSocketSession session, ChatMessage chatMessage, ChatService chatService) {
-        if (chatMessage.getType().equals(ChatMessage.MessageType.ENTER)) {
-            sessions.add(session);
-            chatMessage.setMessage(chatMessage.getSender() + "님이 입장했습니다.");
-        }
-        sendMessage(chatMessage, chatService);
-    }
-
-    public <T> void sendMessage(T message, ChatService chatService) {
-        sessions.parallelStream().forEach(session -> chatService.sendMessage(session, message));
+    public static ChatRoom create(String name) {
+        ChatRoom chatRoom = new ChatRoom();
+        chatRoom.roomId = UUID.randomUUID().toString();
+        chatRoom.name = name;
+        return chatRoom;
     }
 }
--- src/main/java/com/server/chatting/controller/ChatController.java
+++ src/main/java/com/server/chatting/controller/ChatController.java
@@ -1,26 +1,21 @@
 package com.server.chatting.controller;
 
-import com.server.chatting.chat.ChatRoom;
-import com.server.chatting.chat.ChatService;
+import com.server.chatting.chat.ChatMessage;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Controller;
 
 @RequiredArgsConstructor
-@RestController
-@RequestMapping("/chat")
+@Controller
 public class ChatController {
 
-    private final ChatService chatService;
+    private final SimpMessageSendingOperations messagingTemplate;
 
-    @PostMapping
-    public ChatRoom createRoom(@RequestParam String name) {
-        return chatService.createRoom(name);
-    }
-
-    @GetMapping
-    public List<ChatRoom> findAllRoom() {
-        return chatService.findAllRoom();
+    @MessageMapping("/chat/message")
+    public void message(ChatMessage message) {
+        if (ChatMessage.MessageType.JOIN.equals(message.getType()))
+            message.setMessage(message.getSender() + "님이 입장하셨습니다.");
+        messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
     }
 }
