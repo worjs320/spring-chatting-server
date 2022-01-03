7f2214cc97a01fe7f62aa31cb85ccaa34bf65224
worjs320 <worjs320@naver.com>
2022-01-04 01:02:01 +0900

    migration: spring 2.4.3 -> 2.6.2

--- src/main/java/com/server/chatting/config/WebSockConfig.java
+++ src/main/java/com/server/chatting/config/WebSockConfig.java
@@ -22,7 +22,8 @@ public class WebSockConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-stomp").withSockJS();
+        registry.addEndpoint("/ws-stomp").setAllowedOriginPatterns("*").withSockJS();
+        // sock.js를 통하여 낮은 버전의 브라우저에서도 websocket이 동작할수 있게 합니다.
     }
 
     @Override
